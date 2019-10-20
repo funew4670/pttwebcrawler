741a3dbab7f107e9d2ed1ca9008e85fffdde81a9
test <funew4670@gmail.com>
2019-10-20 13:26:44 +0000

    loop pttweb hardwaresale by params add filter by key word 'sale'

---
+++ pttwebserarch.py
@@ -0,0 +1,57 @@
+from requests_html import HTML
+import requests
+
+
+def parse_article_entries(doc):
+
+    html = HTML(html=doc)
+
+    post_entries = html.find('div.r-ent')
+
+    return post_entries
+
+def fetch(url,params):
+
+    #response = requests.get(url,params)
+    
+    response = requests.get(url,params=params, cookies={'over18': '1'})
+
+    return response
+
+def parse_article_meta(entry):
+
+    meta = {'title': entry.find('div.title', first=True).text,
+        'push': entry.find('div.nrec', first=True).text,
+        'date': entry.find('div.date', first=True).text,
+        'author': entry.find('div.author', first=True).text,
+        'link': entry.find('div.title > a', first=True).attrs['href'],
+    }
+    try:
+        meta['author'] = entry.find('div.author', first=True).text
+        meta['link'] = entry.find('div.title > a', first=True).attrs['href']
+    except AttributeError:
+        if '(本文已被刪除)' in meta['title']:
+            match_author = re.search('\[(\w*)\]', meta['title'])
+            if match_author:
+                meta['author'] = match_author.group(1)
+        elif re.search('已被\w*刪除', meta['title']):
+            match_author = re.search('\<(\w*)\>', meta['title'])
+            if match_author:
+                meta['author'] = match_author.group(1)
+    return meta
+    
+    return meta
+
+
+url = 'https://www.ptt.cc/bbs/HardwareSale/search'
+params = ["1060","650w"]
+
+for para in params:
+    resp = fetch(url,{'q':para})  # step-1
+    post_entries = parse_article_entries(resp.text)  # step-2
+    for entry in post_entries:
+        meta = parse_article_meta(entry)
+        if "賣" not in meta['title'] :
+            continue
+        print(meta)  # result of setp-3
+
